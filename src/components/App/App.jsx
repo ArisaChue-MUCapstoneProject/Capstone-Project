7c46a1166e3e8c916c071b3def0357b80a1045c9
arisachue <arisachue@gmail.com>
2022-08-01 01:28:00 -0700

    css styling for signup profile page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -24,6 +24,7 @@ export default function App() {
   // state variables
   const [users, setUsers] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [creatingAccount, setCreatingAccount] = useState(true)
 
   // listen for realtime updates and update users
   useEffect(() => {
@@ -45,10 +46,10 @@ export default function App() {
     <div className="App">
         <BrowserRouter>
           <main className="main">
-            {currentUser && <Navbar />}
+            {currentUser && !creatingAccount && <Navbar />}
             <Routes>
               <Route path="/signup" element={<SignUp />} />
-              <Route path="/signup/profile" element={<SignUpProfile users={users} isLoading={isLoading} />} />
+              <Route path="/signup/profile" element={<SignUpProfile users={users} isLoading={isLoading} setCreatingAccount={setCreatingAccount}/>} />
               <Route path="/login" element={<LogIn />} />
               <Route path="/forgot-password" element={<ForgotPass />} />
               <Route path="/marketplace" element={<PrivateRoute><MarketPlace users={users} isLoading={isLoading} /></PrivateRoute>} />
