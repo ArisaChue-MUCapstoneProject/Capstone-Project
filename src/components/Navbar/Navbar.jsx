fcc354c98c479c05ff57dfaef1a908b8baa00c8a
arisachue <arisachue@gmail.com>
2022-07-31 22:22:35 -0700

    Misc font changes, navbar link location changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,20 +10,20 @@ export default function Navbar() {
       <Link to="/">
           <p id="navbar-links">Log Out</p>
       </Link>
-      <Link to="/marketplace">
-        <p id="navbar-links">Marketplace</p>
-      </Link>
       <Link to="/profile">
           <p id="navbar-links">Profile</p>
-      </Link>
-      <Link to="/recipes">
-          <p id="navbar-links">Recipes</p>
       </Link>
       <Link to="/pantry">
           <p id="navbar-links">Pantry</p>
       </Link>
+      <Link to="/recipes">
+          <p id="navbar-links">Recipes</p>
+      </Link>
       <Link to="/shoppingcart">
           <p id="navbar-links">Grocery List</p>
+      </Link>
+      <Link to="/marketplace">
+        <p id="navbar-links">Marketplace</p>
       </Link>
     </nav>
   )
